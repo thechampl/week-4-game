ab49e977b093fd5b7282b0819a873fb81bd39512
thechampl <luke.champ@outlook.com>
2018-01-06 09:35:14 -0500

    draft, need to fix scoring system and win-loss

--- assets/game.js
+++ assets/game.js
@@ -1,32 +1,4 @@
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-
-$(document).ready(function(){ 
-       
-    
-   
-
-  {/* Global Variables */}
-  var wins = 0
-  var loses = 0
-  var totalscore = 0
-  var randomnumber = math.floor((Math.random() * 50) + 30);
-  var crystalnumberone = []
-  var crystalnumbertwo = []
-  var crystalnumberthree = []
-  var crystalnumberfour = []
-
-  var crystalnumbers = [1, 2, 3, 5]
-  
-  document.getElementById("randomnumber").textContent = randomnumber;
-
-  document.onclick = function (event) {
-
-      console.log(randomnumber)
-  }
-  
-});
- 
   
   
   
